9a1ae8617ddf648e076577b1aa010497e3ee2fd7
mehul1409 <mehulbansalswm1234@gmail.com>
2025-03-17 10:25:08 +0530

    add contet

---
+++ models/add.js
@@ -0,0 +1,28 @@
+import { json } from "express";
+
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+// Load environment variables from .env file
+dotenv.config();
+
+
+// Initialize express app
+const app = express();
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, (err) => {
+    if (err) {
+        console.error('❌ Error while starting the server:', err);
+    } else {
+        console.log(`✅ Server is listening on port ${PORT}`);
+    }
+});
